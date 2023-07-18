bf2c6909fa16e49d2b9bdedcfe80fe4f5ef24bcb
arepa_flipper <cristian00tovar@gmail.com>
2023-07-18 11:58:39 -0500

    🎑 (58:32) set a simple navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,26 @@
 import { useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import { themeSettings } from "./theme";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from '@/scenes/Navbar';
 
 const App = () => {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>dashboard page</div>} />
+              <Route path="/predictions" element={<div>preditctions page</div>} />
+            </Routes>
+          </Box >
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 };
