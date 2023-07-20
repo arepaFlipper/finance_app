08d662744a5c764781e16cd209f8b731989a48de
arepa_flipper <cristian00tovar@gmail.com>
2023-07-20 11:34:19 -0500

    🏩 (1:30:15) set Dashboard Rows

---
+++ client/src/scenes/Dashboard/Row3.tsx
@@ -0,0 +1,15 @@
+import DashboardBox from './DashboardBox';
+type Props = {}
+
+function Row3({ }: Props) {
+  return (
+    <>
+      <DashboardBox gridArea="g"></DashboardBox>
+      <DashboardBox gridArea="h"></DashboardBox>
+      <DashboardBox gridArea="i"></DashboardBox>
+      <DashboardBox gridArea="j"></DashboardBox>
+    </>
+  )
+}
+
+export default Row3;
--- client/src/scenes/Dashboard/index.tsx
+++ client/src/scenes/Dashboard/index.tsx
@@ -1,9 +1,10 @@
 import { Box, Typography, useMediaQuery } from '@mui/material';
 import { gridTemplateLargeScreens, gridTemplateSmallScreens } from './gridTemplates';
-import DashboardBox from './DashboardBox';
+import Row1 from './Row1';
+import Row2 from './Row2';
+import Row3 from './Row3';
 
-type Props = {}
-const Dashboard = (props: Props) => {
+const Dashboard = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
   return (
     <Box width="100%" height="100%" display="grid" gap="1.5rem"
@@ -15,16 +16,9 @@ const Dashboard = (props: Props) => {
         gridTemplateAreas: gridTemplateSmallScreens
       }}
     >
-      <DashboardBox gridArea="a"></DashboardBox>
-      <DashboardBox gridArea="b"></DashboardBox>
-      <DashboardBox gridArea="c"></DashboardBox>
-      <DashboardBox gridArea="d"></DashboardBox>
-      <DashboardBox gridArea="e"></DashboardBox>
-      <DashboardBox gridArea="f"></DashboardBox>
-      <DashboardBox gridArea="g"></DashboardBox>
-      <DashboardBox gridArea="h"></DashboardBox>
-      <DashboardBox gridArea="i"></DashboardBox>
-      <DashboardBox gridArea="j"></DashboardBox>
+      <Row1 />
+      <Row2 />
+      <Row3 />
     </Box>
   )
 }
